91d43558c81bdc32058f658e6e5e7917eeba97ec
Le Duc <ltdletrungduc@gmail.com>
2021-06-01 17:18:10 +0700

    FIRST COMMIT

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(window).on("load", function () {
+// 	myLoading();
+// });
+// function myLoading() {
+// 	setTimeout(() => {
+// 		$("body > .overlay").addClass("loaded");
+// 		AOS.init();
+// 	}, 1234);
+// }
+svgToInline(".inline-svg");
